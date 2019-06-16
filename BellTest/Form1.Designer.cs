4e65c35b36b6aefae18c6619dc624a87c646d340
Longoon <longoon12000@gmail.com>
2019-06-16 10:59:28 +0200

    Ring ding ding ding ding

---
+++ BellTest/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace BellTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(409, 426);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(427, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(508, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(427, 12);
+            this.trackBar1.Maximum = 300;
+            this.trackBar1.Minimum = 15;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(361, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 15;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(586, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(427, 92);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 346);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Templates";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Taizé";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Glocken";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ BellTest/Form1.cs
@@ -0,0 +1,256 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace BellTest
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> bells = new Dictionary<string, string>()
+        {
+            { "a0.wav", "A0" },
+            { "a1.wav", "A1" },
+            { "a2.wav", "A2" },
+            { "ais-b0.wav", "B0" },
+            { "ais-b1.wav", "B1" },
+            { "ais-b2.wav", "B2" },
+            { "C0.wav", "C0" },
+            { "c1.wav", "C1" },
+            { "c2.wav", "C2" },
+            { "cis-des0.wav", "C#0" },
+            { "cis-des1.wav", "C#1" },
+            { "cis-des2.wav", "C#2" },
+            { "d0.wav", "D0" },
+            { "d1.wav", "D1" },
+            { "d2.wav", "D2" },
+            { "dis-es0.wav", "D#0" },
+            { "dis-es1.wav", "D#1" },
+            { "dis-es2.wav", "D#2" },
+            { "e0.wav", "E0" },
+            { "e1.wav", "E1" },
+            { "e2.wav", "E2" },
+            { "f0.wav", "F0" },
+            { "f1.wav", "F1" },
+            { "f2.wav", "F2" },
+            { "fis-ges0.wav", "F#0" },
+            { "fis-ges1.wav", "F#1" },
+            { "fis-ges2.wav", "F#2" },
+            { "g0.wav", "G0" },
+            { "g1.wav", "G1" },
+            { "g2.wav", "G2" },
+            { "gis-as0.wav", "G#0" },
+            { "gis-as1.wav", "G#1" },
+            { "gis-as2.wav", "G#2" },
+            { "h0.wav", "H0" },
+            { "h1.wav", "H1" },
+            { "h2.wav", "H2" }
+        };
+
+        private Dictionary<string, MediaPlayer> players;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.trackBar1.Value = 60;
+            _ = this.loadBells();
+        }
+
+        private async Task loadBells()
+        {
+            this.players = this.bells.ToDictionary(kvp => kvp.Key, kvp => new MediaPlayer());
+            foreach (string path in this.players.Keys)
+            {
+                this.players[path].Stop();
+                this.players[path].Volume = 0.0;
+                this.players[path].Open(new Uri(path, UriKind.Relative));
+            }
+
+            while (this.players.Values.Count(p => p.BufferingProgress < 1.0) > 0)
+                await Task.Delay(500);
+
+            foreach (string path in this.players.Keys)
+            {
+                this.players[path].Volume = 1.0;
+
+                this.flowLayoutPanel1.Controls.Add(new CheckBox()
+                {
+                    Text = this.bells[path]
+                });
+            }
+
+            this.button1.Enabled = true;
+            this.button2.Enabled = true;
+        }
+
+
+        private string GetBellByNote(BellNote note) => this.bells.First(b => b.Value == note.ToString().Replace('s', '#')).Key;
+        private BellNote GetNoteByName(string name) => (BellNote)Enum.Parse(typeof(BellNote), Enum.GetNames(typeof(BellNote)).First(n => n.Replace('s', '#') == name));
+        private static readonly char[] NOTE_ORDER = new[]
+        {
+            'C',
+            'D',
+            'E',
+            'F',
+            'G',
+            'A',
+            'B',
+            'H'
+        };
+
+        CancellationTokenSource primaryCancellation;
+        CancellationTokenSource cancellation;
+
+        private static BellNote[] OrderNotes(BellNote[] notes) => notes.OrderBy(b => int.Parse(b.ToString().Substring(b.ToString().Length - 1)) * 10 + Array.IndexOf(NOTE_ORDER, b.ToString()[0])).ToArray();
+
+        private async Task ring(BellNote[] notes, TimeSpan duration)
+        {
+            BellNote[] orderedNotes = OrderNotes(notes).Reverse().ToArray();
+            BellNote startingBell = orderedNotes[0];
+            if (orderedNotes.Length > 1)
+                startingBell = orderedNotes[1];
+            orderedNotes = orderedNotes.Where(n => n != startingBell).ToArray();
+
+            this.primaryCancellation = new CancellationTokenSource(duration + WINDDOWN_DELAY);
+            _ = this.loopBell(startingBell, this.primaryCancellation.Token);
+            int delay = 2500;
+            await Task.Delay(delay);
+
+            if (orderedNotes.Length > 0)
+            {
+                this.cancellation = new CancellationTokenSource(duration);
+                int leftElement = 0;
+                int rightElement = orderedNotes.Length - 1;
+
+                for (int i = 0; i < orderedNotes.Length; i++)
+                {
+                    int noteIndex = i % 2 == 0 ? leftElement++ : rightElement--;
+                    _ = this.loopBell(orderedNotes[noteIndex], cancellation.Token);
+                    delay = Math.Max(650, delay - 500);
+                    await Task.Delay(delay);
+                }
+            }
+
+            while (!this.primaryCancellation.IsCancellationRequested)
+                await Task.Delay(500);
+
+            Console.WriteLine("This is where we stop");
+            this.button1.Enabled = true;
+        }
+
+        private async Task loopBell(BellNote note, CancellationToken token)
+        {
+            Console.WriteLine($"Playing {note}");
+            MediaPlayer player = this.players[GetBellByNote(note)];
+            while (!token.IsCancellationRequested)
+            {
+                player.Position = new TimeSpan(0);
+                player.Play();
+                await Task.Delay((int)player.NaturalDuration.TimeSpan.TotalMilliseconds);
+            }
+            Console.WriteLine($"Winding down {note}");
+            player.Position = new TimeSpan(0);
+            player.Play();
+            while (true)
+            {
+                player.Volume = Math.Max(player.Volume - 0.025, 0);
+                Console.WriteLine($"Reduced {note} to {player.Volume * 100.0}% volume");
+                await Task.Delay(250);
+                if (player.Volume == 0)
+                    break;
+            }
+            Console.WriteLine($"Ended {note}");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            BellNote[] selectedBells = this.flowLayoutPanel1.Controls.OfType<CheckBox>().Where(c => c.Checked).Select(c => GetNoteByName(c.Text)).ToArray();
+
+            this.button1.Enabled = false;
+            _ = this.ring(selectedBells, new TimeSpan(0, 0, this.trackBar1.Value));
+        }
+
+        private void TrackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            TimeSpan duration = new TimeSpan(0, 0, this.trackBar1.Value);
+            this.label1.Text = $"Duration: {duration.Minutes} minutes and {duration.Seconds} seconds";
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            this.applyTemplate(new[] { BellNote.F1, BellNote.B1, BellNote.C2, BellNote.D2, BellNote.F2 });
+        }
+
+        private static readonly TimeSpan WINDDOWN_DELAY = new TimeSpan(0, 0, 3);
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.cancellation?.Cancel();
+            this.primaryCancellation?.CancelAfter(WINDDOWN_DELAY);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            this.applyTemplate(new[] { BellNote.F2, BellNote.Ds1, BellNote.Gs1 });
+        }
+
+        private void applyTemplate(BellNote[] template)
+        {
+            List<CheckBox> bellBoxes = this.flowLayoutPanel1.Controls.OfType<CheckBox>().ToList();
+            bellBoxes.ForEach(b => b.Checked = false);
+            bellBoxes.Where(b => template.Contains(GetNoteByName(b.Text))).ToList().ForEach(b => b.Checked = true);
+        }
+    }
+
+    enum BellNote
+    {
+        A0,
+        A1,
+        A2,
+        B0,
+        B1,
+        B2,
+        C0,
+        C1,
+        C2,
+        Cs0,
+        Cs1,
+        Cs2,
+        D0,
+        D1,
+        D2,
+        Ds0,
+        Ds1,
+        Ds2,
+        E0,
+        E1,
+        E2,
+        F0,
+        F1,
+        F2,
+        Fs0,
+        Fs1,
+        Fs2,
+        G0,
+        G1,
+        G2,
+        Gs0,
+        Gs1,
+        Gs2,
+        H0,
+        H1,
+        H2
+    }
+}
